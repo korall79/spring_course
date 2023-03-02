8987b3d12822bf554f0a806d5d511af0989ee45d
korall79 <koralll79@yandex.ru>
2023-03-02 17:08:48 +0400

    spring

---
+++ src/hibernate_test/Test1.java
@@ -0,0 +1,16 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory()
+
+
+    }
+}
